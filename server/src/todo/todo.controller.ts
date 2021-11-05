639ba2bb423c0fd678bf459c4e9361c0d78f1a17
Jerilene Medrano <jerixmx@gmail.com>
2021-11-05 23:03:51 +0800

    fix(controller): correct constructor call
    + linting

--- server/src/todo/todo.controller.ts
+++ server/src/todo/todo.controller.ts
@@ -1,22 +1,31 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { toPromise } from 'src/shared/utils';
 import { TodoCreateDto } from './dto/todo.create.dto';
 import { TodoDto } from './dto/todo.dto';
 import { TodoListDto } from './dto/todo.list.dto';
+import { TodoService } from './todo.service';
 
 @Controller('api/todos')
 export class TodoController {
-  constructor(private readonly todoService: TodoService{});
-  
+  constructor(private readonly todoService: TodoService) {}
+
   @Get()
   async findAll(): Promise<TodoListDto> {
     const todos = await this.todoService.getAllTodo();
 
-    return toPromise({todos});
+    return toPromise({ todos });
   }
 
-  @Get(":id")
-  async findOne(@Param("id") id: string): Promise<TodoDto> {
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<TodoDto> {
     return await this.todoService.getOneTodo(id);
   }
 
@@ -25,13 +34,16 @@ export class TodoController {
     return await this.todoService.createTodo(todoCreateDto);
   }
 
-  @Put(":id")
-  async update(@Param("id") id: string, @Body() todoDto: TodoDto): Promise<TodoDto> {
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() todoDto: TodoDto,
+  ): Promise<TodoDto> {
     return await this.todoService.updateTodo(id, todoDto);
   }
 
-  @Delete(":id")
-  async destroy(@Param("id") id: string): Promise<TodoDto> {
+  @Delete(':id')
+  async destroy(@Param('id') id: string): Promise<TodoDto> {
     return await this.todoService.destroyTodo(id);
-  }  
+  }
 }
