10e9822772671554b2688880087f913001fea075
Jerilene Medrano <jerixmx@gmail.com>
2021-11-06 22:40:42 +0800

    feat(todo): import entities to the module

--- server/src/todo/todo.module.ts
+++ server/src/todo/todo.module.ts
@@ -3,9 +3,13 @@ import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
 import { TaskController } from './task/task.controller';
 import { TaskService } from './task/task.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { TaskEntity } from './entity/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TodoEntity, TaskEntity])],
   controllers: [TodoController, TaskController],
-  providers: [TodoService, TaskService]
+  providers: [TodoService, TaskService],
 })
 export class TodoModule {}
