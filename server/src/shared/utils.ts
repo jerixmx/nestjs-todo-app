a0df660dda6696cbe726a333c8eed2810a5a1d76
Jerilene Medrano <jerixmx@gmail.com>
2021-11-06 19:31:24 +0800

    feat(db): add typeorm helper methods

--- server/src/shared/utils.ts
+++ server/src/shared/utils.ts
@@ -1,5 +1,27 @@
+import { getConnectionOptions, getConnection } from 'typeorm';
+
 export const toPromise = <T>(data: T): Promise<T> => {
   return new Promise<T>((resolve) => {
     resolve(data);
   });
+};
+
+export const getDbConnectionOptions = async (connectionName = 'default') => {
+  const options = await getConnectionOptions(
+    process.env.NODE_ENV || 'development',
+  );
+
+  return {
+    ...options,
+    name: connectionName,
+  };
+};
+
+export const getDbConnection = async (connectionName = 'default') => {
+  return await getConnection(connectionName);
+};
+
+export const runDbMigrations = async (connectionName = 'default') => {
+  const conn = await getDbConnection(connectionName);
+  await conn.runMigrations();
 };
