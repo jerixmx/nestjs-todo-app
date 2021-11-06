ec2d1e63d90c807c4373cecd5d733952aa80720a
Jerilene Medrano <jerixmx@gmail.com>
2021-11-06 21:36:02 +0800

    feat(task): create controller

--- server/src/todo/todo.module.ts
+++ server/src/todo/todo.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TodoController } from './todo.controller';
 import { TodoService } from './todo.service';
+import { TaskController } from './task/task.controller';
 
 @Module({
-  controllers: [TodoController],
+  controllers: [TodoController, TaskController],
   providers: [TodoService]
 })
 export class TodoModule {}
