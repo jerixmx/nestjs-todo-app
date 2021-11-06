eb4a61328c288fc6d0ae7a383f894eff05df2a75
Jerilene Medrano <jerixmx@gmail.com>
2021-11-06 21:29:05 +0800

    feat(task): add entity to dto mapper

--- server/src/shared/mapper.ts
+++ server/src/shared/mapper.ts
@@ -1,11 +1,26 @@
+import { TaskDto } from 'src/todo/dto/task.dto';
 import { TodoDto } from 'src/todo/dto/todo.dto';
+import { TaskEntity } from 'src/todo/entity/task.entity';
 import { TodoEntity } from 'src/todo/entity/todo.entity';
 
 export const toTodoDto = (data: TodoEntity): TodoDto => {
-  const { id, name, description } = data;
+  const { id, name, description, tasks } = data;
 
-  // Will be reassigned when tasks are incorporated
   let todoDto: TodoDto = { id, name, description };
 
+  if (tasks) {
+    todoDto = {
+      ...todoDto,
+      tasks: tasks.map((task: TaskEntity) => toTaskDto(task)),
+    };
+  }
   return todoDto;
+};
+
+export const toTaskDto = (data: TaskEntity): TaskDto => {
+  const { id, name } = data;
+
+  const taskDto: TaskDto = { id, name };
+
+  return taskDto;
 };
