af43984ba94cb3403526d30e159a382f8e8b41c5
Jerilene Medrano <jerixmx@gmail.com>
2021-11-06 19:42:08 +0800

    feat(db): transform AppModule to DynamicModule to accept connection settings

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConnectionOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
-@Module({
-  imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+@Module({})
+export class AppModule {
+  static forRoot(connOptions: ConnectionOptions): DynamicModule {
+    return {
+      module: AppModule,
+      imports: [TodoModule, TypeOrmModule.forRoot(connOptions)],
+      controllers: [AppController],
+      providers: [AppService],
+    };
+  }
+}
