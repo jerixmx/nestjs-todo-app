d9bdf135a98f67fe0d2eb1957bf6dbb6868b7df7
Jerilene Medrano <jerixmx@gmail.com>
2021-11-06 19:56:12 +0800

    feat(db): pass connection options to AppModule
    Also trigger DB migration during bootstrap

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,17 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common/services/logger.service';
+import { getDbConnectionOptions, runDbMigrations } from '@shared/utils';
 
 const port = process.env.PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(
+    AppModule.forRoot(await getDbConnectionOptions(process.env.NODE_ENV)),
+  );
+
+  await runDbMigrations();
+
   await app.listen(port);
 
   Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
