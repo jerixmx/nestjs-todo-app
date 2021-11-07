e2a7f5b6f34de3e1d21f390dc415d93c9fa30660
Jerilene Medrano <jerixmx@gmail.com>
2021-11-07 15:22:11 +0800

    feat(auth): set up the authentication module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule]
+  imports: [UsersModule, AuthModule]
 })
 export class AppModule {
   static forRoot(connOptions: ConnectionOptions): DynamicModule {
---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.register({
+      secret: process.env.SECRETKEY,
+      signOptions: {
+        expiresIn: process.env.EXPIRESIN,
+      },
+    }),
+  ],
+  exports: [PassportModule, JwtModule],
+})
+export class AuthModule {}
