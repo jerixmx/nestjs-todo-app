a9a493eaff8e909d46af6e66704fff79d30809b4
Jerilene Medrano <jerixmx@gmail.com>
2021-11-07 16:51:04 +0800

    feat: add owner column to the todo table

--- server/src/todo/entity/todo.entity.ts
+++ server/src/todo/entity/todo.entity.ts
@@ -4,15 +4,18 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { UserEntity } from 'src/users/entity/user.entity';
 
 @Entity('todo')
 export class TodoEntity {
   @PrimaryGeneratedColumn('uuid') id: string;
   @Column({ type: 'varchar', nullable: false }) name: string;
   @Column({ type: 'text', nullable: false }) description?: string;
+  @ManyToOne((type) => UserEntity) owner?: UserEntity;
   @CreateDateColumn() createdOn?: Date;
   @CreateDateColumn() updatedOn?: Date;
   @OneToMany(() => TaskEntity, (task) => task.todo) tasks?: TaskEntity[];
