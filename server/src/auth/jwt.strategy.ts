550e69622932d6132735baf76304acef817a1e43
Jerilene Medrano <jerixmx@gmail.com>
2021-11-07 16:06:52 +0800

    feat(auth): create jwt strategy for returning user validation info

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/interfaces/payload.interface';
+import { UserDto } from 'src/users/dto/user.dto';
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRETKEY,
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<UserDto> {
+    const user = await this.authService.validateUser(payload);
+    if (!user) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    return user;
+  }
+}
